38a3297b7a9119fde9432c47c3f34e188b0f3f5b
treneese <treneesejohnson9012@gmail.com>
2024-05-20 20:04:16 -0400

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+
+    def get_title(self):  
+        return self._title
     
-        
+    def set_title(self, new_title):
+        if isinstance(new_title, str):
+            self._title = new_title
+        else:
+            raise TypeError('Title must be of type str')
+    title = property(get_title, set_title)
+
+   
+    def get_page_count(self):
+        return self._page_count
+    
+    
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count, set_page_count)
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
+
+import ipdb
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand =brand
+        self.size = size
+
+    def get_brand(self):
+        return self.brand
+    
+    def set_brand(self, new_brand):
+        if isinstance(new_brand, str):
+            self.brand = new_brand
+        else:
+            raise TypeError('Brand must be of type str')
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self.size
+    
+    def set_size(self,new_size):
+        if isinstance(new_size,int):
+            self.size = new_size
+        else:
+            print("size must be an integer")
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+    
+
+ipdb.set_trace()
